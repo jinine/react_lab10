65b561d5e01bda8778301908e358b5cc4687bc29
Tristan Engen <tengen@my.bcit.ca>
2022-03-23 13:55:52 -0700

    fixing privacy link

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,7 +16,7 @@ const NavBar = () => (
               <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/Privacy">Privacy</a>
+              <Link to="/Privacy">Privacy</Link>
             </li>
           </ul>
         
